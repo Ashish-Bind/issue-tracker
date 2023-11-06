0ae46c8e0551d0e784adf593886e8a1aafcd1b23
Ashish <officialsandwork@gmail.com>
2023-11-06 11:56:21 +0530

    Added Prsima and API for Issues

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -14,15 +14,13 @@ const Navbar = () => {
 
   return (
     <nav className="flex justify-between px-8 border-b p-4 border-gray-300">
-      <p className="text-xl">
-        <Bug />
-      </p>
-      <ul className="flex gap-6">
+      <p className="text-2xl">🐞</p>
+      <ul className="flex gap-6 items-center">
         {links.map((link) => (
           <li
             key={link.href}
-            className={`font-medium text-base text-zinc-500 hover:text-zinc-900 ${
-              path === link.href ? 'underline text-zinc-900' : ''
+            className={`font-medium text-base  hover:text-zinc-900 ${
+              path === link.href ? 'underline text-zinc-900' : 'text-zinc-400'
             }`}
           >
             <Link href={link.href}>{link.name}</Link>
